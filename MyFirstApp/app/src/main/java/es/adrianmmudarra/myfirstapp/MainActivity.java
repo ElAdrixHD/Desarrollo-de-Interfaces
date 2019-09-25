4c9a11c6e6a3357e22f2cbe8bf85e3af648ef257
Adrian Muñoz <eladrixhd@gmail.com>
2019-09-25 09:54:23 +0200

    Added a lucario :3

--- MyFirstApp/app/src/main/java/es/adrianmmudarra/myfirstapp/MainActivity.java
+++ MyFirstApp/app/src/main/java/es/adrianmmudarra/myfirstapp/MainActivity.java
@@ -2,12 +2,32 @@ package es.adrianmmudarra.myfirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * ACTIVITY CON LA FOTO DE LUCARIO
+ * @author Adrián Muñoz Mudarra
+ * @version 0.1;
+ */
 
 public class MainActivity extends AppCompatActivity {
+
+    //Variable con el objeto del TextView de la vista
+    private TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialize();
+        textView.setText(R.string.pkmnGO);
+    }
+
+    /**
+     * METODO DONDE SE INICIALIZA LAS VARIABLES U OBJETOS;
+     */
+    private void initialize(){
+        textView = findViewById(R.id.tv_lucario);
     }
 }
