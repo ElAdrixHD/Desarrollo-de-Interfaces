6c2f33f9b930864174bc531d08ba203fb2d89577
Adrian Muñoz <eladrixhd@gmail.com>
2019-10-09 10:11:48 +0200

    Terminado Recursos

--- HelloWorldResources/app/src/main/java/es/adrianmmudarra/helloworldresources/MainActivity.java
+++ HelloWorldResources/app/src/main/java/es/adrianmmudarra/helloworldresources/MainActivity.java
@@ -1,14 +1,27 @@
 package es.adrianmmudarra.helloworldresources;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tvText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialize();
+
+    }
+
+    private void initialize() {
+        tvText = findViewById(R.id.tvText);
+        tvText.setTextColor(ContextCompat.getColor(this,android.R.color.white));
+        tvText.setTypeface(Typeface.createFromAsset((getAssets()),"Raphtalia.otf"));
     }
 }
