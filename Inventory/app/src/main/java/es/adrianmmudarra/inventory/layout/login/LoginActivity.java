270e6f05fb96abebbf45f2fb3f80c2e34e471cbb
Adrian Muñoz <eladrixhd@gmail.com>
2019-10-10 11:14:49 +0200

    Empezando Inventory

---
+++ Inventory/app/src/main/java/es/adrianmmudarra/inventory/layout/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package es.adrianmmudarra.inventory.layout.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import es.adrianmmudarra.inventory.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
