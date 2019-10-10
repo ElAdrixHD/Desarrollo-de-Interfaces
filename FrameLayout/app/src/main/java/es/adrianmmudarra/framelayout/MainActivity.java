4100a570877cbfe262e7ad373520445e3d83f8e8
Adrian Muñoz <eladrixhd@gmail.com>
2019-10-10 09:17:52 +0200

    Terminado FrameLayout

--- FrameLayout/app/src/main/java/es/adrianmmudarra/framelayout/MainActivity.java
+++ FrameLayout/app/src/main/java/es/adrianmmudarra/framelayout/MainActivity.java
@@ -3,12 +3,34 @@ package es.adrianmmudarra.framelayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnVisibility;
+    private TextView tvText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialize();
+    }
+
+    private void initialize() {
+        tvText = findViewById(R.id.tvText);
+        btnVisibility = findViewById(R.id.btnVisibility);
+        btnVisibility.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnVisibility:
+                tvText.setVisibility((tvText.getVisibility()==View.VISIBLE)?View.INVISIBLE:View.VISIBLE);
+                break;
+        }
     }
 }
