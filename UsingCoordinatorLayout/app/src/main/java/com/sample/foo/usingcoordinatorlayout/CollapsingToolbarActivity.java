54adb1d0c4e5d1e43fab7d351fbd8e1bf22f475a
Adrian Muñoz <eladrixhd@gmail.com>
2020-01-21 10:27:37 +0100

    Proyectos

---
+++ UsingCoordinatorLayout/app/src/main/java/com/sample/foo/usingcoordinatorlayout/CollapsingToolbarActivity.java
@@ -0,0 +1,22 @@
+package com.sample.foo.usingcoordinatorlayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+
+public class CollapsingToolbarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collapsing_toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+}
